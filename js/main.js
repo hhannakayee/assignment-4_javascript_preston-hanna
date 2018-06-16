ff7db2e6bd58e0dc2f6b72a2d36683c3acb4a17c
Hanna Preston <hkpreston92@bellsouth.net>
2018-06-16 16:54:38 -0400

    add presidents

--- js/main.js
+++ js/main.js
@@ -1,2 +1,45 @@
-var iceCream = ["Rocky Road", "Chocolate Chip Cookie Dough", "Mint"];
-iceCream.push("Cherry Chocolate");
+var presidents = [
+
+  {
+    firstName: 'Donald',
+    lastName: 'Trump',
+    termLength: 1,
+    party: 'Republican',
+    yearsOfPresidency: '2017-2020'
+  },
+
+  {
+    firstName: 'Barack',
+    lastName: 'Obama',
+    termLength: 2,
+    party: 'Democrat',
+    yearsOfPresidency: '2009-2016'
+  },
+
+  {
+    firstName: 'George W.',
+    lastName: 'Bush',
+    termLength: 2,
+    party: 'Republican',
+    yearsOfPresidency: '2001-2008'
+  },
+
+  {
+    firstName: 'Bill',
+    lastName: 'Clinton',
+    termLength: 2,
+    party: 'Democrat',
+    yearsOfPresidency: '1993-2000'
+  },
+
+  {
+    firstName: 'George H.W.',
+    lastName: 'Bush',
+    termLength: 1,
+    party: 'Republican',
+    yearsOfPresidency: '1989-1992'
+  },
+
+];
+
+console.log(presidents[2].firstName + ' ' + presidents[2].lastName);
